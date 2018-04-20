6326d0e25dbbbc943b0b72448f89f7679d57f968
orionjeong <djflqjfl0209@gmail.com>
2018-04-20 15:17:46 +0900

    exam1

--- src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -1,18 +1,24 @@
 package kr.ac.jejunu;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.annotation.Bean;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class ProductDaoTest {
+    private ProductDao productDao;
+    @Before
     public void setup() {
+        productDao = new ProductDao();
     }
 
     @Test
     public void get() throws SQLException, ClassNotFoundException {
-        ProductDao productDao = new ProductDao();
         Long id = 1L;
         String title = "제주감귤";
         Integer price = 15000;
@@ -21,5 +27,20 @@ public class ProductDaoTest {
         assertEquals(id, product.getId());
         assertEquals(title, product.getTitle());
         assertEquals(price, product.getPrice());
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        Product product = new Product();
+        product.setTitle("한라감귤");
+        product.setPrice(10000);
+
+        long id = productDao.insert(product);
+
+        Product insertProduct = productDao.get(id);
+
+        assertThat(insertProduct.getTitle(), is(product.getTitle()));
+        assertThat(insertProduct.getPrice(), is(product.getPrice()));
+        assertThat(insertProduct.getId(), is(id));
     }
 }
